1eb7766f81dc7a8ea5e508c5b36d41f2565eb648
ashu <ashutosh.dubey@aviconncorp.com>
2025-12-04 13:07:56 +0530

    chore: enable no-console ESLint rule to enforce LoggerService usage

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -18,6 +18,7 @@ module.exports = [
 		linterOptions: { reportUnusedDisableDirectives: true },
 		rules: {
 			"@typescript-eslint/no-explicit-any": "off",
+			"no-console": "error",
 			"@angular-eslint/directive-selector": ["error", { "type": "attribute", "prefix": "app", "style": "camelCase" }],
 			"@angular-eslint/component-selector": ["error", { "type": "element", "prefix": "app", "style": "kebab-case" }]
 		}
